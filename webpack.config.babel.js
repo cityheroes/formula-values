b805bd5d16d01bdde8fa8f4120d96f2b7f18e205
Jorge Luis Arteaga Almaraz <jorge@pop-os.localdomain>
2018-05-18 11:18:32 -0400

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,22 @@
+import path from 'path';
+
+export default () => ({
+	entry: './src/FormulaValue.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'FormulaValue.js',
+		libraryTarget: 'umd',
+		library: 'FormulaValue'
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}]
+	},
+	stats: {
+		colors: true
+	},
+	devtool: 'source-map'
+});
