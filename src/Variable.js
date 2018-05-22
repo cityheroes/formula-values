4fb83bd99cd4f3e39105578362142ac96fe88eb5
Jorge Luis Arteaga Almaraz <jorge@pop-os.localdomain>
2018-05-22 11:17:25 -0400

    Missing file

---
+++ src/Variable.js
@@ -0,0 +1,96 @@
+import _ from 'underscore';
+import Helpers from './Helpers';
+
+const variableRegex = /\[(?!(?:@|\*|\d+)\]|[\.$])|^[^\[]*\]|\][^\[]*\]|[\{\}]/;
+
+export default class Variable {
+
+	constructor(text) {
+
+
+		if (variableRegex.test(text)) {
+			text = '';
+		}
+
+		this._hasContext = false;
+
+		let tempPath = Helpers.getPath(text);
+
+		let tempPathElement;
+		for (let i = tempPath.length - 1; i >= 0; i--) {
+			tempPathElement = tempPath[i];
+			if (tempPathElement !== '@' && tempPathElement !== '*') {
+				tempPath[i] = tempPathElement;
+			} else {
+				this._hasContext = true;
+			}
+		}
+
+		this._path = tempPath;
+
+		if (text.indexOf('::') > -1) {
+			this._environment = Helpers.metaDataVarName;
+		} else {
+			this._environment = Helpers.dataVarName;
+		}
+
+		if (text === '') {
+			this._parsedVariable = 'null';
+		} else if (!this._hasContext) {
+			let parsedVariable = this._environment;
+			for (var i = 0, len = tempPath.length; i < len; i++) {
+				parsedVariable += '[\'' + tempPath[i] + '\']';
+			}
+			this._parsedVariable = parsedVariable;
+		}
+	}
+
+	parseVariable(contextPath) {
+		return (this._parsedVariable || this._parseWithEnvironment(contextPath));
+	}
+
+	_parseWithEnvironment(contextPath) {
+		let index = 0,
+			contextLength = contextPath.length,
+			pathLength = this._path.length,
+			fieldPath = this._path;
+
+		for (; index<contextLength && index < pathLength; index++) {
+			if (fieldPath[index] === '@' && _.isNumber(contextPath[index])) {
+				fieldPath[index] = Number(contextPath[index]);
+			} else if (fieldPath[index] !== contextPath[index] || fieldPath[index] === '*') {
+				break;
+			}
+		}
+		for (; index < pathLength; index++) {
+			if (fieldPath[index] === '@') {
+				throw new Error('Context could not fully resolve');
+			} else if (fieldPath[index] === '*') {
+				fieldPath[index] = '*';
+			}
+		}
+
+		let pathElement,
+			hasStarOperator = false,
+			parsedVariable = this._environment;
+		for (var i = 0, len = this._path.length; i < len; i++) {
+			pathElement = this._path[i];
+			if (pathElement === '*') {
+				if (hasStarOperator) {
+					parsedVariable += '")';
+				} else {
+					hasStarOperator = true;
+				}
+				parsedVariable = '__processStarOperator(' + parsedVariable + ',"';
+			} else {
+				parsedVariable += '[\'' + pathElement + '\']';
+			}
+		}
+
+		if (hasStarOperator) {
+			parsedVariable += '")';
+		}
+		return parsedVariable;
+	}
+
+}
