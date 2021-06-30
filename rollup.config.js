579ca02194118bc60c4bc1cd82b15fc5ac00bf48
Jorge Luis Arteaga Almaraz <jorgitoaral@gmail.com>
2021-06-30 04:24:09 -0400

    chore: Added named iife build for browsers

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
+
+export default {
+  input: './build/module/index.js',
+  output: {
+    file: './build/web/formula-values.js',
+    format: 'iife',
+    name: 'FormulaValues'
+  },
+  treeshake: false,
+  plugins: [
+    resolve(),
+    commonJS({
+      include: [
+        'node_modules/dayjs/dayjs.min.js',
+        'node_modules/dayjs/plugin/advancedFormat.js',
+        'node_modules/dayjs/plugin/utc.js',
+        'node_modules/lodash/lodash.js',
+      ]
+    })
+  ]
+};
